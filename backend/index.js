0fa493397f4d40aeb627938768e7413c1925ddfa
ManabBala <manabbalaofficial@gmail.com>
2024-07-01 11:18:20 +0530

    added processing of results and moved images and csv to main storage

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express, { json, response } from "express";
 import multer from "multer";
 import { faker } from "@faker-js/faker";
 import path from "path";
+import fse from "fs-extra";
 
 const app = express();
 
@@ -15,7 +16,7 @@ const storage = multer.diskStorage({
 		cb(null, "./backend/uploads");
 	},
 	filename: function (req, file, cb) {
-		cb(null, file.fieldname + "-" + faker.string.nanoid(10) + path.extname(file.originalname));
+		cb(null, faker.string.nanoid(10) + path.extname(file.originalname));
 	},
 });
 
@@ -30,61 +31,138 @@ app.get("/", async function (req, res) {
 // file upload route
 app.post("/upload", upload.single("file"), function (req, res) {
 	const filePath = req.file.path;
+	const fileName = req.file.filename;
+	console.log("File Name: " + fileName);
 
-	console.log(` filePath: ${filePath}\n `);
+	console.log(`File Path: ${filePath}\n `);
 	res.json({ msg: "file uploaded", error: 0 });
 
-	// TODO: processing the uploaded file further
+	// processing the uploaded file further
+	processUploadedFile(fileName, filePath);
 });
 
-const processUploadedFile = (URI) => {
+const processUploadedFile = (fileName, URI) => {
 	console.log("processing file: " + URI);
 
 	// TODO: logic to check if its a valid image
 
 	// TODO: cleanup the python OMR processing folders
 
-	// TODO: move image to the python OMR input folder
+	// copy image to the python OMR input folder
+	let srcPath = URI;
+	let destPath = "./backend/utils/python/storage/inputs/" + fileName;
+	try {
+		fse.copySync(srcPath, destPath, { overwrite: true }); // overwrite if already exists
+		console.log("successfully copy file from " + srcPath + " to " + destPath);
+
+		// run the python script
+		runPythonScript()
+			.then(() => {
+				console.log("Python Script Executed Successfully");
+
+				// TODO: check if results are valid or something..
+
+				console.log("Starting processing results");
+				// process the results from the python script
+				processResults(fileName, URI);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+const processResults = (fileName, URI) => {
+	console.log("processing results");
+	// making omr storage folder with the name of the image by faker
+	let omrResultStoragePath = "./backend/storage/" + path.parse(URI).name;
+	fse.ensureDirSync(omrResultStoragePath);
+
+	// move omr to the storage folder
+	let srcPath = "./backend/utils/python/storage/inputs/" + fileName;
+	let destPath = omrResultStoragePath + "/OMR.jpg";
+	try {
+		fse.moveSync(srcPath, destPath, { overwrite: true }); // overwrite if already exists
+		console.log("successfully moved OMR from " + srcPath + " to " + destPath);
+	} catch (err) {
+		console.error(err);
+	}
+
+	// move checkedOMR to the storage folder
+	srcPath = "./backend/utils/python/storage/outputs/CheckedOMRs/" + fileName;
+	destPath = omrResultStoragePath + "/checkedOMR.jpg";
+	try {
+		fse.moveSync(srcPath, destPath, { overwrite: true }); // overwrite if already exists
+		console.log("successfully moved checkedOMR from " + srcPath + " to " + destPath);
+	} catch (err) {
+		console.error(err);
+	}
+
+	// find all the csv files in the output/Results folder
+	let resultsFileName = "";
+	let directoryPath = "./backend/utils/python/storage/outputs/Results";
+	try {
+		const files = fse.readdirSync(directoryPath);
+		if (!Array.isArray(files)) {
+			console.log(files);
+			return console.error("Expected files to be an array, but got:", typeof files);
+		}
+		const csvFiles = files.filter((file) => path.extname(file).toLowerCase() === ".csv");
+		console.log("CSV files:", csvFiles);
+
+		// TODO: need proper way to select the latest csv file
+		resultsFileName = csvFiles[0];
+	} catch (err) {
+		console.error("Unable to scan directory:", err);
+	}
+
+	// move results.csv to the storage folder
+	srcPath = "./backend/utils/python/storage/outputs/Results/" + resultsFileName;
+	destPath = omrResultStoragePath + "/result.csv";
+	try {
+		fse.moveSync(srcPath, destPath, { overwrite: true }); // overwrite if already exists
+		console.log("successfully moved results.csv from " + srcPath + " to " + destPath);
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 // python script will be executed here
 const runPythonScript = () => {
-	let options = {
-		pythonPath: "backend/utils/python/python_env/Scripts/python.exe",
-		mode: "text",
-		pythonOptions: ["-u"], // get print results in real-time
-		scriptPath: "backend/utils/python/OMRChecker", //If you are having python_test.py script in same folder, then it's optional.
-		args: [
-			"-i",
-			"backend/utils/python/storage/inputs/",
-			"-o",
-			"backend/utils/python/storage/outputs/",
-		], //An argument which can be accessed in the script using sys.argv[1]
-	};
+	return new Promise((resolve, reject) => {
+		// run the python script
+		console.log("Running python script...");
+		let options = {
+			pythonPath: "backend/utils/python/python_env/Scripts/python.exe",
+			mode: "text",
+			pythonOptions: ["-u"], // get print results in real-time
+			scriptPath: "backend/utils/python/OMRChecker", //If you are having python_test.py script in same folder, then it's optional.
+			args: [
+				"-i",
+				"backend/utils/python/storage/inputs/",
+				"-o",
+				"backend/utils/python/storage/outputs/",
+			], //An argument which can be accessed in the script using sys.argv[1]
+		};
 
-	let pyshell = new PythonShell("main.py", options);
+		let pyshell = new PythonShell("main.py", options);
 
-	// sends a message to the Python script via stdin
-	// pyshell.send({ name: "Manab", age: 24 });
+		// sends a message to the Python script via stdin
+		// pyshell.send({ name: "Manab", age: 24 });
 
-	pyshell.on("message", function (message) {
-		// received a message sent from the Python script (a simple "print" statement)
-		// try {
-		// 	if (!(message.search("Booklet_No") === -1)) {
-		// 		message = message.replace(/'/g, '"');
-		// 		let responseData = JSON.parse(message);
-		// 		console.log(responseData);
-		// 	}
-		// } catch (error) {
-		// 	console.log(error);
-		// }
-		console.log("[Python]", message);
-	});
+		pyshell.on("message", function (message) {
+			console.log("[Python]", message);
+		});
 
-	// end the input stream and allow the process to exit
-	pyshell.end(function (err, code, signal) {
-		if (err) throw err;
-		console.log("The exit code was: " + code);
+		// end the input stream and allow the process to exit
+		pyshell.end(function (err, code, signal) {
+			console.log("The exit code was: " + code);
+			if (err) reject(err);
+			// resolve the promise
+			resolve();
+		});
 	});
 };
 
